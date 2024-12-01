e6211e35855e6ffcbeeba2e5d59da7151f56ac9e
Aleksandar <al.stojanovic0@gmail.com>
2024-12-01 23:32:32 +0100

    Checkpoint

---
+++ app/src/main/java/com/incognito/gasstationpos/MyReceiver.java
@@ -0,0 +1,38 @@
+package com.incognito.gasstationpos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.incognito.gasstationpos.utils.MyApp;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("RECEIVER", "Intent received (including ECR response): " + intent.getAction());
+
+        if(MyApp.currentActivity instanceof MainActivity){
+            Log.d("RECEIVER", "Main activity already started");
+            Intent i = new Intent(context, MainActivity.class);
+            i.setAction(intent.getAction());
+            if (intent.getStringExtra("ResponseResult") != null && intent.getStringExtra("ResponseResult") != "") {
+                i.putExtra("ResponseResult", intent.getStringExtra("ResponseResult"));
+            }
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            context.startActivity(i);
+        }
+        else if(MyApp.currentActivity == null){
+            Log.d("RECEIVER", "Start main activity");
+            Intent i = new Intent(context, MainActivity.class);
+            i.setAction(intent.getAction());
+            if (intent.getStringExtra("ResponseResult") != null && intent.getStringExtra("ResponseResult") != "") {
+                i.putExtra("ResponseResult", intent.getStringExtra("ResponseResult"));
+            }
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
